bcab59c1b248f8f1e372b1ed4e80bf33db5d1f89
leehsihau <34845784+leehsihau@users.noreply.github.com>
2021-07-31 07:08:46 -0500

    Express Router With Rest API

--- node-express/index.js
+++ node-express/index.js
@@ -3,12 +3,18 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-
+const dishRouter = require('./routes/dishRouter');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/dishes', dishRouter)
+
+
+
 
 app.use(express.static(__dirname+'/public'))
 
