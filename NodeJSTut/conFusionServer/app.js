d31fe8de9ec54dbe2d810dd27610f2eadde0b3dd
leehsihau <34845784+leehsihau@users.noreply.github.com>
2021-08-04 18:45:57 -0500

    Node and mongodb

--- NodeJSTut/conFusionServer/app.js
+++ NodeJSTut/conFusionServer/app.js
@@ -7,6 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//custom routers
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter)
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
