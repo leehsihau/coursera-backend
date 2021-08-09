0db9946e36b79e4add73270bbd71b24d9b46454e
leehsihau <34845784+leehsihau@users.noreply.github.com>
2021-08-09 10:27:43 -0500

    mongoose pt2.2 Added full usage of comment schema

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -9,22 +9,38 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
     Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
-        .then((dish) => {
-            console.log(dish);
+    .then((dish) => {
+        console.log(dish);
 
-            return Dishes.find({}).exec();
-        })/* 
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })/* 
         .then((dishes) => {
             console.log(dishes);
 
             return Dishes.remove({});
-        })
+        })*/
         .then(() => {
             return mongoose.connection.close();
-        }) */
+        }) 
         .catch((err) => {
             console.log(err);
         });
