c5aaa22f55096d38ac62c3239771faa1c5db14e4
leehsihau <34845784+leehsihau@users.noreply.github.com>
2021-09-03 18:13:35 -0500

    Passport Jwt

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -10,6 +10,8 @@ var FileStore = require('session-file-store')(session);
 
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,7 +24,7 @@ var leaderRouter = require('./routes/leaderRouter');
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 
@@ -43,6 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-6'));
 
+/*
 app.use(session({
   name: 'session-id',
   secret: '123456-7',
@@ -50,13 +53,14 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
-
+*/
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+/*
 function auth(req, res, next) {
   console.log(req.user);
 
@@ -70,6 +74,7 @@ function auth(req, res, next) {
   }
 }
 app.use(auth);
+*/
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const leaderRouter = express.Router();
+var authenticate = require('../authenticate');
 
 const Leaders = require('../models/leaders')
 
@@ -15,7 +16,7 @@ leaderRouter.route('/').get((req, res, next) => {
         }, (err) => next(err))
         .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Leaders.create(req.body)
         .then((leader) => {
             console.log('leader Created ', leader);
@@ -25,11 +26,11 @@ leaderRouter.route('/').get((req, res, next) => {
         }, (err) => next(err))
         .catch((err) => next(err));
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /leaders');
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Leaders.remove({})
         .then((resp) => {
             res.statusCode = 200;
@@ -49,11 +50,11 @@ leaderRouter.route('/:leaderId')
         }, (err) => next(err))
         .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /leaders/' + req.params.leaderId);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Leaders.findByIdAndUpdate(req.params.leaderId, {
         $set: req.body
     }, { new: true })
@@ -64,7 +65,7 @@ leaderRouter.route('/:leaderId')
         }, (err) => next(err))
         .catch((err) => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Leaders.findByIdAndRemove(req.params.leaderId)
         .then((resp) => {
             res.statusCode = 200;
